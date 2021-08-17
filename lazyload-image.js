0a168e03477d5bfd7c240201100b393cf2baaf35
nguyen khanh <64463734+khanhfe@users.noreply.github.com>
2021-08-17 16:54:39 +0700

    Update lazyload-image.js

--- lazyload-image.js
+++ lazyload-image.js
@@ -1,1 +1,14 @@
+let lazy = $('.lazy-load'); // tranh goi DOM nhieu lan gay anh huong den hieu suat
+function lazy_img(){
+    lazy.each(function() {
+        if( $(this).offset().top <= ($(window).scrollTop() + $(window).height()) && // dieu kien hinh anh nam trong viewport moi load vao src
+        	$(this).offset().top >= $(window).scrollTop() && // dieu kien hinh anh nam trong viewport moi load vao src
+        	$(this).attr('src') == null ){
+            	let src = $(this).attr('data-src');
+            	$(this).attr('src', src);
+        }
+    });
 
+    return false; // 
+
+}
