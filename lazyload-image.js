7c2845765aa9c93aa98587ae9dc352aaf6774b46
nguyen khanh <64463734+khanhfe@users.noreply.github.com>
2021-08-17 16:55:50 +0700

    edit comment
    edit detail comment

--- lazyload-image.js
+++ lazyload-image.js
@@ -1,8 +1,8 @@
 let lazy = $('.lazy-load'); // tranh goi DOM nhieu lan gay anh huong den hieu suat
 function lazy_img(){
     lazy.each(function() {
-        if( $(this).offset().top <= ($(window).scrollTop() + $(window).height()) && // dieu kien hinh anh nam trong viewport moi load vao src
-        	$(this).offset().top >= $(window).scrollTop() && // dieu kien hinh anh nam trong viewport moi load vao src
+        if( $(this).offset().top <= ($(window).scrollTop() + $(window).height()) && // dieu kien hinh anh nam trong viewport moi load vao src (dang scroll len thi can dk nay)
+        	$(this).offset().top >= $(window).scrollTop() && // dieu kien hinh anh nam trong viewport moi load vao src (dang scroll xuong thi can dk nay)
         	$(this).attr('src') == null ){
             	let src = $(this).attr('data-src');
             	$(this).attr('src', src);
